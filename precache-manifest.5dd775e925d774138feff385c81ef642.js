d03b9a5e80ad5883919d2211bf24cd2b4daa4c8e
Low Jeng Lam <jenglamlow@gmail.com>
2020-07-18 01:16:33 +0800

    Updates

---
+++ precache-manifest.5dd775e925d774138feff385c81ef642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a871efc5b33a6533d367e11de936c3",
+    "url": "/url-interceptor/index.html"
+  },
+  {
+    "revision": "096377f636e1fbbbc481",
+    "url": "/url-interceptor/static/css/main.736ae46f.chunk.css"
+  },
+  {
+    "revision": "1da8944caf8e4e0b35f8",
+    "url": "/url-interceptor/static/js/2.679aa8ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/url-interceptor/static/js/2.679aa8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096377f636e1fbbbc481",
+    "url": "/url-interceptor/static/js/main.d2f2a021.chunk.js"
+  },
+  {
+    "revision": "a3d49d277209100e0849",
+    "url": "/url-interceptor/static/js/runtime-main.2e78cbc5.js"
+  }
+]);
